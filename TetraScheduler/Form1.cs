7771225158dd32053f0077ecb47b62bcebabdc3d
knrichter19 <knrichter19@coe.edu>
2022-01-31 15:02:50 -0600

    basic login ui with username/password fields

--- TetraScheduler/Form1.cs
+++ TetraScheduler/Form1.cs
@@ -17,5 +17,14 @@ namespace TetraScheduler
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // function to log in
+        }
     }
 }
