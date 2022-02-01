43b13d9ec0cff22584711119fc8a84900206ad45
knrichter19 <knrichter19@coe.edu>
2022-02-01 17:18:09 -0600

    grabbing username/password from login boxes, error message

--- TetraScheduler/Form1.cs
+++ TetraScheduler/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace TetraScheduler
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +26,21 @@ namespace TetraScheduler
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // function to log in
+            String username = textBox1.Text;
+            String password = textBox2.Text;
+
+            if (username.Length == 0 || password.Length == 0){
+                MessageBox.Show("Username/Password cannot be empty!");
+            }
+
+            else {
+                // validate username/password from database here - show error if incorrect, otherwise load appropriate dashboard
+                // todo: password validation function + file setup
+            }
+
+            // remove this later - debugging only
+            Debug.WriteLine("Username: " + textBox1.Text + "\tPassword: " + textBox2.Text);
+
         }
     }
 }
