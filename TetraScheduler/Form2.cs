f10ed685faaa3e4560f01040f9c1edb75f13923a
knrichter19 <knrichter19@coe.edu>
2022-02-02 14:33:34 -0600

    form 2 passing username + template functions

---
+++ TetraScheduler/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TetraScheduler
+{
+    public partial class Form2 : Form
+    {
+        public Form2(String username)
+        {
+            InitializeComponent();
+            Debug.WriteLine("Loading " + username + "'s data");
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
