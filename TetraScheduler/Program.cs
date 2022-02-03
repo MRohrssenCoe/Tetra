9a95c1160938420b425f8144e96b82bd677ef56d
Michael Rohrssen <marohrssen19@coe.edu>
2022-02-03 00:17:33 -0800

    Added creation of a dummy account for first time use.

--- TetraScheduler/Program.cs
+++ TetraScheduler/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text.Encodings;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,6 +23,15 @@ namespace TetraScheduler
             string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string specificFolder = Path.Combine(folder, "TetraScheduler");
             Directory.CreateDirectory(specificFolder);
+            string pswdFile = Path.Combine(specificFolder, "SchedulerPasswords.txt");
+            if (!File.Exists(pswdFile))
+            {
+                FileStream fs = File.Open(pswdFile, FileMode.Create);
+                String tempText = "admin, password";
+                fs.Write(System.Text.Encoding.ASCII.GetBytes(tempText), 0, tempText.Length);
+                fs.Close();
+            }
+            
             Application.Run(new Form1());
         }
     }
