8ad448de729075820bc1fb5c0cbcf378cdeef533
knrichter19 <knrichter19@coe.edu>
2022-02-14 08:36:32 -0600

    don't remember what i did here but pulling

--- TetraScheduler/Program.cs
+++ TetraScheduler/Program.cs
@@ -45,7 +45,7 @@ namespace TetraScheduler
 
                 // remove dummy data later
                 // maybe add default shifts for each hour and selectively read them based on open hours
-                sb.AppendLine("Sunday, 2:00, 2:30, Bob");
+                sb.AppendLine("Sunday, 2:00, 2:30, 'Bob, Alice, Jane, Katya, Will, Michael, Ashley'"); // todo: figure out how to write multiple
                 sb.AppendLine("Monday, 1:00, 2:00, Bob");
                 sb.AppendLine("Tuesday, 3:00, 3:30, Bob");
                 sb.AppendLine("Tuesday, 3:30, 4:30, Bob");
@@ -58,6 +58,7 @@ namespace TetraScheduler
                 //fs.Write(System.Text.Encoding.ASCII.GetBytes(tempCSV), 0, tempCSV.Length);
                 //fs.Close();
             }
+
 
             // starts at login form
             Application.Run(new LoginForm());
