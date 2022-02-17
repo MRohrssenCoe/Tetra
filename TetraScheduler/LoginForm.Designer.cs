449e3745367d1a174793beeb35c03b90083ea117
Michael Rohrssen <marohrssen19@coe.edu>
2022-02-16 20:26:34 -0800

    ahhhh

--- TetraScheduler/LoginForm.Designer.cs
+++ TetraScheduler/LoginForm.Designer.cs
@@ -115,7 +115,6 @@ namespace TetraScheduler
             this.pictureBox1.Size = new System.Drawing.Size(163, 41);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // LoginForm
             // 
