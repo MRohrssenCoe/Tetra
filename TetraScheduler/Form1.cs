43450be47451795d6bcbcedb97b1ebc56f3d2aa6
willd <WDROPA20@COE.EDU>
2022-02-18 14:15:57 -0600

    Form1.cs

---
+++ TetraScheduler/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TetraScheduler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // login button functionality
+
+
+            String username = textBox1.Text;
+            String password = textBox2.Text;
+            String FileWrite = "\n";
+
+            if (username.Length == 0 || password.Length == 0)
+            {
+                MessageBox.Show("Username/Password cannot be empty!");
+                // add more error visuals?
+            }
+
+            else
+            {
+                FileWrite = username + "," + password + ",";
+            }
+        }
+    }
+}
