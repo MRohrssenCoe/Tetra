73ea9012219d2b20242e5b89960d67bc8c3105a3
knrichter19 <knrichter19@coe.edu>
2022-02-25 14:48:04 -0600

    nothing big changed

---
+++ TetraScheduler/ScheduleMaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetraScheduler
+{
+    class ScheduleMaker
+    {
+
+        public ScheduleMaker()
+        {
+
+        }
+
+
+        
+    }
+}
