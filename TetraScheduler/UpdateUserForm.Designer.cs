15e4671457b96e941dc5bf4f48d4d2d4b2c870b1
willd <WDROPA20@COE.EDU>
2022-03-28 14:16:43 -0500

    3/28/2022

---
+++ TetraScheduler/UpdateUserForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TetraScheduler
+{
+    partial class UpdateUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(8, 41);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(324, 356);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This form is to change a user\'s status. Who do you want to change?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Make Admin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Make Consultant";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Make Both";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // UpdateUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "UpdateUserForm";
+            this.Text = "UpdateUserForm";
+            this.Load += new System.EventHandler(this.UpdateUserForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ TetraScheduler/UpdateUserForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TetraScheduler
+{
+    public partial class UpdateUserForm : Form
+    {
+        private string[] token;
+        public UpdateUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateUserForm_Load(object sender, EventArgs e)
+        {
+            string tetraFolder = Constants.AppDataFolder;
+            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+            string usernameString = File.ReadAllText(pswdFile);
+            token = usernameString.Split(",");
+            int x = 0;
+            while (x < token.Length)
+            {
+                if (token[x + 2].Equals("0"))
+                {
+                    checkedListBox1.Items.Add(token[x] + " Consultant");
+                }
+                else if (token[x + 2].Equals("1"))
+                {
+                    checkedListBox1.Items.Add(token[x] + " Admin");
+                }
+                else if (token[x + 2].Equals("3"))
+                {
+                    checkedListBox1.Items.Add(token[x] + " Admin and Consultant");
+                }
+                x += 3;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+            foreach(int x in i)
+            {
+                token[(3 * x) + 2] = "0";
+            }
+            String tokenStr = "";
+            for(int y = 0; y < token.Length - 1; y++)
+            {
+                tokenStr = tokenStr + token[y] + ",";
+            }
+            string tetraFolder = Constants.AppDataFolder;
+            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+            tokenStr = tokenStr + token[token.Length - 1];
+            File.WriteAllText(pswdFile, tokenStr);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+            foreach (int x in i)
+            {
+                token[(3 * x) + 2] = "3";
+            }
+            String tokenStr = "";
+            for (int y = 0; y < token.Length - 1; y++)
+            {
+                tokenStr = tokenStr + token[y] + ",";
+            }
+            string tetraFolder = Constants.AppDataFolder;
+            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+            tokenStr = tokenStr + token[token.Length - 1];
+            File.WriteAllText(pswdFile, tokenStr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+            foreach (int x in i)
+            {
+                jsonPath = token[x * 3] + ".json";
+                token[(3 * x) + 2] = "3";
+            }
+            String tokenStr = "";
+            for (int y = 0; y < token.Length - 1; y++)
+            {
+                tokenStr = tokenStr + token[y] + ",";
+            }
+            string tetraFolder = Constants.AppDataFolder;
+            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+            tokenStr = tokenStr + token[token.Length - 1];
+            File.WriteAllText(pswdFile, tokenStr);
+        }
+    }
+}
