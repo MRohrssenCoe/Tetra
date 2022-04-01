d2c4976631d21233d623859ff4c5f61a37bb0a65
willd <WDROPA20@COE.EDU>
2022-04-01 14:25:32 -0500

    Update users complete

--- TetraScheduler/UpdateUserForm.cs
+++ TetraScheduler/UpdateUserForm.cs
@@ -12,6 +12,8 @@ namespace TetraScheduler
     public partial class UpdateUserForm : Form
     {
         private string[] token;
+        private int[] admins;
+        private int adminCount;
         public UpdateUserForm()
         {
             InitializeComponent();
@@ -28,7 +30,9 @@ namespace TetraScheduler
             string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
             string usernameString = File.ReadAllText(pswdFile);
             this.token = usernameString.Split(",");
+            Array.Resize(ref admins, token.Length);
             int x = 0;
+            int y = 0;
             while (x < token.Length)
             {
                 if (token[x + 2].Equals("0"))
@@ -38,6 +42,8 @@ namespace TetraScheduler
                 else if (token[x + 2].Equals("1"))
                 {
                     checkedListBox1.Items.Add(token[x] + " Admin");
+                    admins[y] = x / 3;
+                    y++;
                 }
                 else if (token[x + 2].Equals("3"))
                 {
@@ -45,67 +51,113 @@ namespace TetraScheduler
                 }
                 x += 3;
             }
+            adminCount = y;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
-            foreach(int x in i)
+            DialogResult dialogResult = MessageBox.Show("You are about to change these user(s) to Consultant. Are you sure?", "Confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
             {
-                token[(3 * x) + 2] = "0";
+                CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+                int a = 0;
+                int b = 0;
+                int c = 0;
+                while(a < i.Count)
+                {
+                    while(b < adminCount)
+                    {
+                        if (i[a].Equals(admins[b]))
+                        {
+                            c++;
+                        }
+                        b++;
+                    }
+                    a++;
+                }
+                if (c == adminCount)
+                {
+                    MessageBox.Show("ERROR \nYou are attempting to delete all Admin accounts.\nProcess cannot be executed.");
+                    this.Close();
+                }
+                else if (c > 0)
+                {
+                    DialogResult dialogResult1 = MessageBox.Show("Warning.\nYou are attempting to delete Admin accounts. \nAre you truly sure?", "Confirmation", MessageBoxButtons.YesNo);
+                    if (dialogResult1 == DialogResult.No)
+                        this.Close();
+                }
+                else
+                {
+                    foreach (int x in i)
+                    {
+                        token[(3 * x) + 2] = "0";
+                    }
+                    String tokenStr = "";
+                    for (int y = 0; y < token.Length - 1; y++)
+                    {
+                        tokenStr = tokenStr + token[y] + ",";
+                    }
+                    string tetraFolder = Constants.AppDataFolder;
+                    string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+                    tokenStr = tokenStr + token[token.Length - 1];
+                    File.WriteAllText(pswdFile, tokenStr);
+                    this.Close();
+                }
             }
-            String tokenStr = "";
-            for(int y = 0; y < token.Length - 1; y++)
-            {
-                tokenStr = tokenStr + token[y] + ",";
-            }
-            string tetraFolder = Constants.AppDataFolder;
-            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
-            tokenStr = tokenStr + token[token.Length - 1];
-            File.WriteAllText(pswdFile, tokenStr);
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
-            foreach (int x in i)
+            DialogResult dialogResult = MessageBox.Show("You are about to change these user(s) to both Admin and Consultant. Are you sure?", "Confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
             {
-                token[(3 * x) + 2] = "3";
+                CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+                foreach (int x in i)
+                {
+                    token[(3 * x) + 2] = "3";
+                }
+                String tokenStr = "";
+                for (int y = 0; y < token.Length - 1; y++)
+                {
+                    tokenStr = tokenStr + token[y] + ",";
+                }
+                string tetraFolder = Constants.AppDataFolder;
+                string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+                tokenStr = tokenStr + token[token.Length - 1];
+                File.WriteAllText(pswdFile, tokenStr);
+                this.Close();
             }
-            String tokenStr = "";
-            for (int y = 0; y < token.Length - 1; y++)
-            {
-                tokenStr = tokenStr + token[y] + ",";
-            }
-            string tetraFolder = Constants.AppDataFolder;
-            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
-            tokenStr = tokenStr + token[token.Length - 1];
-            File.WriteAllText(pswdFile, tokenStr);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
-            foreach (int x in i)
+            DialogResult dialogResult = MessageBox.Show("You are about to change these user(s) to Admin. Are you sure?", "Confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
             {
-                String jsonName = token[x * 3] + ".json";
-                String Pathhalf = Constants.userPreferencesFolder;
-                String jsonPath = Path.Combine(Pathhalf, jsonName);
-                File.Delete(jsonPath);
+                CheckedListBox.CheckedIndexCollection i = checkedListBox1.CheckedIndices;
+                foreach (int x in i)
+                {
+                    String jsonName = token[x * 3] + ".json";
+                    String Pathhalf = Constants.userPreferencesFolder;
+                    String jsonPath = Path.Combine(Pathhalf, jsonName);
+                    File.Delete(jsonPath);
+                }
+                foreach (int x in i)
+                {
+                    token[(3 * x) + 2] = "1";
+                }
+                String tokenStr = "";
+                for (int y = 0; y < token.Length - 1; y++)
+                {
+                    tokenStr = tokenStr + token[y] + ",";
+                }
+                string tetraFolder = Constants.AppDataFolder;
+                string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
+                tokenStr = tokenStr + token[token.Length - 1];
+                File.WriteAllText(pswdFile, tokenStr);
+                this.Close();
             }
-            foreach (int x in i)
-            {
-                token[(3 * x) + 2] = "1";
-            }
-            String tokenStr = "";
-            for (int y = 0; y < token.Length - 1; y++)
-            {
-                tokenStr = tokenStr + token[y] + ",";
-            }
-            string tetraFolder = Constants.AppDataFolder;
-            string pswdFile = Path.Combine(tetraFolder, Constants.passwordFileName);
-            tokenStr = tokenStr + token[token.Length - 1];
-            File.WriteAllText(pswdFile, tokenStr);
         }
     }
 }
