7b02e63f063ae641dca413502dc485b31e0ce31d
Michael Rohrssen <marohrssen19@coe.edu>
2022-04-04 14:36:39 -0500

    branch with working admin xd

---
+++ TetraScheduler/WarningForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TetraScheduler
+{
+    partial class WarningForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The following consultants have not all of their hours filled: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(396, 198);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(12, 305);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(396, 198);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "The following shifts have too few or too many consultants:";
+            // 
+            // WarningForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 515);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "WarningForm";
+            this.Text = "WarningForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ TetraScheduler/WarningForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TetraScheduler
+{
+    public partial class WarningForm : Form
+    {
+        public WarningForm()
+        {
+            InitializeComponent();
+        }
+        public WarningForm(List<(UserInfo, int)> userTuple, List<(Shift, int)> shiftsTuple)
+        {
+            InitializeComponent();
+            fillUsersList(userTuple);
+            fillShiftsList(shiftsTuple);
+
+        }
+
+        private void fillShiftsList(List<(Shift, int)> shiftsTuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void fillUsersList(List<(UserInfo, int)> userTuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
